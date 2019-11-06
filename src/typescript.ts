d3d737728a1bf9fa69ba87205065a599139efa93
Zachary Belford <belfordz66@gmail.com>
2019-11-05 16:32:56 -0800

    feat: new typings lib

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,95 +1,41 @@
 import {
   Generator,
-  GetSchemaTypings,
   GetMethodTypings,
+  GetMethodAliasName,
 } from "./generator-interface";
-import _ from "lodash";
-import { compile } from "json-schema-to-typescript";
 import { ContentDescriptorObject, MethodObject, Schema } from "@open-rpc/meta-schema";
-import { collectAndRefSchemas, getSchemaTypeName, getMethodAliasName, getSchemasForOpenRPCDocument } from "./utils";
+import { languageSafeName, ensureSchemaTitles } from "@etclabscore/json-schema-to-types/build/utils";
 
-const isComment = (line: string): boolean => {
-  const trimmed = line.trim();
-  return _.startsWith(trimmed, "/**") || _.startsWith(trimmed, "*") || _.startsWith(trimmed, "*/");
-};
-
-const getDefs = (lines: string): string => {
-  let commentBuffer: string[] = [];
-  return _.chain(lines.split("\n"))
-    .reduce((memoLines: any[], line) => {
-      const lastItem = _.last(memoLines);
-      const singleLine = line.match(/export (.*);/);
-
-      if (isComment(line)) {
-        commentBuffer.push(line);
-      } else if (singleLine) {
-        memoLines.push([...commentBuffer, line]);
-        commentBuffer = [];
-      } else {
-        const isStruct = line.match(/export (.*)/);
-        if (isStruct) {
-          memoLines.push([...commentBuffer, line]);
-          commentBuffer = [];
-        } else if (_.isArray(lastItem)) {
-          lastItem.push(line);
-          if (line === "}") {
-            memoLines.push("");
-          }
-        }
-      }
-
-      return memoLines;
-    }, [])
-    .compact()
-    .uniqBy((exportLine) => {
-      const toTest = exportLine instanceof Array ?
-        _.reject(exportLine, isComment)[0] : exportLine;
-      const [all, exportType, name, rest] = toTest.match(/export\s(type|interface|enum)\s(\S*)/);
-      return name;
-    })
-    .flattenDeep()
-    .join("\n")
-    .value();
-};
-
-const compileOpts = { bannerComment: "", declareExternallyReferenced: false };
-
-export const getSchemaTypings: GetSchemaTypings = async (openrpcDocument) => {
-  const schemas = getSchemasForOpenRPCDocument(openrpcDocument);
-
-  const rawTypes = await Promise.all(schemas.map((s: Schema) => compile(s, "", compileOpts)));
-
-  const trimmedAndJoined = _.map(rawTypes, _.trim).join("\n").trim();
-  const defs = getDefs(trimmedAndJoined);
-  return defs;
+export const getMethodAliasName: GetMethodAliasName = (method) => {
+  return languageSafeName(method.name);
 };
 
 const getMethodTyping = (method: MethodObject): string => {
   const result = method.result as ContentDescriptorObject;
-  const resultTypeName = `Promise<${getSchemaTypeName(result.schema)}>`;
+  const resultName = ensureSchemaTitles({ ...result.schema });
+  const resultTypeName = `Promise<${languageSafeName(resultName.title)}>`;
 
   const methodAliasName = getMethodAliasName(method);
 
-  const params = _.map(
-    method.params as ContentDescriptorObject[],
-    (param) => `${param.name}${param.required ? "" : "?"}: ${getSchemaTypeName(param.schema)}`,
+  const params = (method.params as ContentDescriptorObject[]).map(
+    (param) => [
+      `${param.name}${param.required ? "" : "?"}: `,
+      `${languageSafeName(ensureSchemaTitles(param.schema).title)}`,
+    ].join(""),
   ).join(", ");
 
   return `export type ${methodAliasName} = (${params}) => ${resultTypeName};`;
 };
 
 export const getMethodTypings: GetMethodTypings = (openrpcDocument) => {
-  return _.chain(openrpcDocument.methods)
+  return openrpcDocument.methods
     .map((method) => getMethodTyping(method))
-    .join("\n")
-    .value();
+    .join("\n");
 };
 
 const generator: Generator = {
   getMethodAliasName,
   getMethodTypings,
-  getSchemaTypeName,
-  getSchemaTypings,
 };
 
 export default generator;
--- src/utils.ts
+++ src/utils.ts
@@ -1,74 +1,21 @@
 import { Schema, MethodObject, OpenRPC, ContentDescriptorObject } from "@open-rpc/meta-schema";
-import _ from "lodash";
-import { GetSchemaTypeName } from "./generator-interface";
-import { toSafeString } from "json-schema-to-typescript/dist/src/utils";
 
-const schemaToRef = (s: Schema) => ({ $ref: `#/definitions/${getSchemaTypeName(s)}` });
-
-export const getMethodAliasName = ({ name }: MethodObject): string => {
-  return getSchemaTypeName({ title: name, type: "function" });
-};
-
-export const getSchemaTypeName = (s: Schema): string => toSafeString(s.title);
-
-export const collectAndRefSchemas = (schema: Schema): Schema[] => {
-  const newS: Schema = { ...schema };
-  const subS: Schema[][] = [];
-
-  if (schema.anyOf) {
-    subS.push(schema.anyOf);
-    newS.anyOf = schema.anyOf.map(schemaToRef);
-  }
-
-  if (schema.allOf) {
-    subS.push(schema.allOf);
-    newS.allOf = schema.allOf.map(schemaToRef);
-  }
-
-  if (schema.oneOf) {
-    subS.push(schema.oneOf);
-    newS.oneOf = schema.oneOf.map(schemaToRef);
-  }
-
-  if (schema.items) {
-    subS.push(schema.items);
-
-    if (schema.items instanceof Array) {
-      newS.items = schema.items.map(schemaToRef);
+const flatten = (arr: Schema[]): Schema[] => {
+  return arr.reduce((memo: Schema[], val) => {
+    if (val instanceof Array) {
+      return [...memo, ...val];
     } else {
-      newS.items = schemaToRef(schema.items);
+      memo.push(val);
+      return memo;
     }
-  }
-
-  if (schema.properties) {
-    subS.push(Object.values(schema.properties));
-    newS.properties = _.mapValues(schema.properties, schemaToRef);
-  }
-
-  const subSchemas: Schema[] = _.chain(subS)
-    .flatten()
-    .compact()
-    .value();
-
-  const collectedSubSchemas: Schema[] = _.map(subSchemas, collectAndRefSchemas);
-
-  return _.chain(collectedSubSchemas)
-    .push([newS])
-    .flatten()
-    .value();
+  }, []);
 };
 
 export const getSchemasForOpenRPCDocument = (openrpcDocument: OpenRPC): Schema[] => {
   const { methods } = openrpcDocument;
 
-  const params = _.flatMap(methods, (method) => method.params as ContentDescriptorObject[]);
-  const result = _.map(methods, (method) => method.result as ContentDescriptorObject);
+  const params = flatten(methods.map((method) => method.params as ContentDescriptorObject[]));
+  const result = methods.map((method) => method.result as ContentDescriptorObject);
 
-  return _.chain(params.concat(result))
-    .map("schema")
-    .map(collectAndRefSchemas)
-    .flatten()
-    .uniqBy("title")
-    .map((schema, i, collection) => ({ ...schema, definitions: _.keyBy(collection, getSchemaTypeName) }))
-    .value();
+  return flatten(params.concat(result).map(({ schema }) => schema));
 };
