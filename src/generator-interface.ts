a145b3f2f7153c7d0f87fb6903a3711ffc24051e
Zachary Belford <belfordz66@gmail.com>
2020-01-15 16:58:45 -0800

    fix: update to latest json-schema-to-typings 1.10.0

--- src/generator-interface.ts
+++ src/generator-interface.ts
@@ -1,4 +1,4 @@
-import { OpenRPC, MethodObject } from "@open-rpc/meta-schema";
+import { OpenrpcDocument as OpenRPC, MethodObject } from "@open-rpc/meta-schema";
 
 export type GetMethodTypings = (openrpcSchema: OpenRPC) => string;
 export type GetMethodAliasName = (method: MethodObject) => string;
